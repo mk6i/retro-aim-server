1398d85b099ec47bed4a14848d011a54122ff51f
Mike <mk6i@proton.me>
2023-06-26 22:01:52 -0400

    refactoring

--- oscar/protocol.go
+++ oscar/protocol.go
@@ -83,6 +83,39 @@ func (s *snacFrame) read(r io.Reader) error {
 	return binary.Read(r, binary.BigEndian, &s.requestID)
 }
 
+type snacWriter interface {
+	write(w io.Writer) error
+}
+
+type TLVPayload struct {
+	TLVs []*TLV
+}
+
+func (s *TLVPayload) read(r io.Reader, lookup map[uint16]reflect.Kind) error {
+	for {
+		// todo, don't like this extra alloc when we're EOF
+		tlv := &TLV{}
+		if err := tlv.read(r, lookup); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		s.TLVs = append(s.TLVs, tlv)
+	}
+
+	return nil
+}
+
+func (s *TLVPayload) write(w io.Writer) error {
+	for _, tlv := range s.TLVs {
+		if err := tlv.write(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 type TLV struct {
 	tType uint16
 	val   any
@@ -264,4 +297,112 @@ func SendAndReceiveSignonFrame(rw io.ReadWriter, sequence uint16) error {
 	fmt.Printf("SendAndReceiveSignonFrame write FLAP: %+v\n", flap)
 
 	return nil
+}
+
+const (
+	OSERVICE      uint16 = 0x0001
+	LOCATE               = 0x0002
+	BUDDY                = 0x0003
+	ICBM                 = 0x0004
+	ADVERT               = 0x0005
+	INVITE               = 0x0006
+	ADMIN                = 0x0007
+	POPUP                = 0x0008
+	PD                   = 0x0009
+	USER_LOOKUP          = 0x000A
+	STATS                = 0x000B
+	TRANSLATE            = 0x000C
+	CHAT_NAV             = 0x000D
+	CHAT                 = 0x000E
+	ODIR                 = 0x000F
+	BART                 = 0x0010
+	FEEDBAG              = 0x0013
+	ICQ                  = 0x0015
+	BUCP                 = 0x0017
+	ALERT                = 0x0018
+	PLUGIN               = 0x0022
+	UNNAMED_FG_24        = 0x0024
+	MDIR                 = 0x0025
+	ARS                  = 0x044A
+)
+
+func ReadBos(rw io.ReadWriter, sequence uint16) error {
+	for {
+		// receive
+		flap := &flapFrame{}
+		if err := flap.read(rw); err != nil {
+			return err
+		}
+
+		b := make([]byte, flap.payloadLength)
+		if _, err := rw.Read(b); err != nil {
+			return err
+		}
+
+		buf := bytes.NewBuffer(b)
+
+		snac := &snacFrame{}
+		if err := snac.read(buf); err != nil {
+			return err
+		}
+
+		switch snac.foodGroup {
+		case OSERVICE:
+		case LOCATE:
+		case BUDDY:
+			if err := routeBuddy(snac, buf, rw); err != nil {
+				return err
+			}
+		case ICBM:
+		case ADVERT:
+		case INVITE:
+		case ADMIN:
+		case POPUP:
+		case PD:
+		case USER_LOOKUP:
+		case STATS:
+		case TRANSLATE:
+		case CHAT_NAV:
+		case CHAT:
+		case ODIR:
+		case BART:
+		case FEEDBAG:
+		case ICQ:
+		case BUCP:
+			if err := routeBUCP(flap, snac, buf, rw, sequence); err != nil {
+				return err
+			}
+			sequence++
+		case ALERT:
+		case PLUGIN:
+		case UNNAMED_FG_24:
+		case MDIR:
+		case ARS:
+		}
+
+	}
+}
+
+func writeOutSNAC(flap *flapFrame, snacFrame snacFrame, snacOut snacWriter, sequence uint16, w io.Writer) error {
+	snacBuf := &bytes.Buffer{}
+	if err := snacFrame.write(snacBuf); err != nil {
+		return err
+	}
+	if err := snacOut.write(snacBuf); err != nil {
+		return err
+	}
+
+	flap.sequence = sequence
+	flap.payloadLength = uint16(snacBuf.Len())
+
+	fmt.Printf(" write FLAP: %+v\n", flap)
+
+	if err := flap.write(w); err != nil {
+		return err
+	}
+
+	fmt.Printf(" write SNAC: %+v\n", snacOut)
+
+	_, err := w.Write(snacBuf.Bytes())
+	return err
 }
