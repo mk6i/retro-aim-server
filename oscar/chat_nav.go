c4bb7febbada94239e7f0cfac5b5bf03eb442739
Mike Kaminski <mike@mkny.net>
2023-08-16 22:32:31 -0400

    consistently asks for create room

--- oscar/chat_nav.go
+++ oscar/chat_nav.go
@@ -45,6 +45,40 @@ func routeChatNav(flap *flapFrame, snac *snacFrame, r io.Reader, w io.Writer, se
 	return nil
 }
 
+type exchangeInfo struct {
+	identifier uint16
+	TLVPayload
+}
+
+func (s *exchangeInfo) write(w io.Writer) error {
+	if err := binary.Write(w, binary.BigEndian, s.identifier); err != nil {
+		return err
+	}
+	//if err := binary.Write(w, binary.BigEndian, uint8(len(s.TLVs))); err != nil {
+	//	return err
+	//}
+	return s.TLVPayload.write(w)
+}
+
+type roomInfo struct {
+	exchange       uint16
+	cookie         []byte
+	instanceNumber uint16
+}
+
+func (s *roomInfo) write(w io.Writer) error {
+	if err := binary.Write(w, binary.BigEndian, s.exchange); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.BigEndian, uint8(len(s.cookie))); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.BigEndian, s.cookie); err != nil {
+		return err
+	}
+	return binary.Write(w, binary.BigEndian, s.instanceNumber)
+}
+
 func SendAndReceiveNextChatRights(flap *flapFrame, snac *snacFrame, r io.Reader, w io.Writer, sequence *uint32) error {
 	fmt.Printf("sendAndReceiveNextChatRights read SNAC frame: %+v\n", snac)
 
@@ -52,15 +86,187 @@ func SendAndReceiveNextChatRights(flap *flapFrame, snac *snacFrame, r io.Reader,
 		foodGroup: CHAT_NAV,
 		subGroup:  ChatNavNavInfo,
 	}
+
+	//rInfo := roomInfo{
+	//	exchange:       4,
+	//	cookie:         []byte("create"),
+	//	instanceNumber: 65535,
+	//}
+	//buf1 := &bytes.Buffer{}
+	//if err := rInfo.write(buf1); err != nil {
+	//	return err
+	//}
+	//
+	//xInfo := exchangeInfo{
+	//	identifier: 4,
+	//	TLVPayload: TLVPayload{
+	//		TLVs: []*TLV{
+	//			{
+	//				tType: 0x05,
+	//				val:   buf1.Bytes(),
+	//			},
+	//			{
+	//				tType: 0x00d1,
+	//				val:   uint16(100),
+	//			},
+	//			{
+	//				tType: 0x000a,
+	//				val:   uint16(0x0114),
+	//			},
+	//			{
+	//				tType: 0x00d2,
+	//				val:   uint16(10),
+	//			},
+	//			{
+	//				tType: 0x02,
+	//				val:   uint16(1),
+	//			},
+	//			{
+	//				tType: 0xc9,
+	//				val:   uint16(63),
+	//			},
+	//			{
+	//				tType: 0xd3,
+	//				val:   "create",
+	//			},
+	//			{
+	//				tType: 0xd5,
+	//				val:   uint8(1),
+	//			},
+	//			{
+	//				tType: 0xd6,
+	//				val:   "us-ascii",
+	//			},
+	//			{
+	//				tType: 0xd7,
+	//				val:   "en",
+	//			},
+	//			{
+	//				tType: 0xd8,
+	//				val:   "us-ascii",
+	//			},
+	//			{
+	//				tType: 0xd9,
+	//				val:   "en",
+	//			},
+	//		},
+	//	},
+	//}
+	//
+	//buf := &bytes.Buffer{}
+	//if err := xInfo.write(buf); err != nil {
+	//	return err
+	//}
+	//
+	//roomBuf := &bytes.Buffer{}
+	//newRoomInfo := &snacCreateRoom{
+	//	exchange:       4,
+	//	cookie:         []byte("create"),
+	//	instanceNumber: 65535,
+	//	detailLevel:    2,
+	//	TLVPayload: TLVPayload{
+	//		TLVs: []*TLV{
+	//			{
+	//				tType: uint16(0xD3),
+	//				val:   "hahah new name",
+	//			},
+	//		},
+	//	},
+	//}
+	//if err := newRoomInfo.write(roomBuf); err != nil {
+	//	return err
+	//}
+
+	xchange := TLVPayload{
+		TLVs: []*TLV{
+			{
+				tType: 0x000a,
+				val:   uint16(0x0114),
+			},
+			//{
+			//	tType: 0x000d,
+			//	val:   nil,
+			//},
+			{
+				tType: 0x0004,
+				val:   uint8(15),
+			},
+			{
+				tType: 0x0002,
+				val:   uint16(0x0010),
+			},
+			{
+				tType: 0x00c9,
+				val:   uint16(15),
+			},
+			{
+				tType: 0x00ca,
+				val:   uint32(time.Now().Unix()),
+			},
+			//{
+			//	tType: 0x00d0,
+			//	val:   nil,
+			//},
+			{
+				tType: 0x00d1,
+				val:   uint16(1024),
+			},
+			{
+				tType: 0x00d2,
+				val:   uint16(100),
+			},
+			{
+				tType: 0x00d3,
+				val:   "hello",
+			},
+			{
+				tType: 0x00d4,
+				val:   "http://www.google.com",
+			},
+			{
+				tType: 0x00d5,
+				val:   uint8(2),
+			},
+			{
+				tType: 0xd6,
+				val:   "us-ascii",
+			},
+			{
+				tType: 0xd7,
+				val:   "en",
+			},
+			{
+				tType: 0xd8,
+				val:   "us-ascii",
+			},
+			{
+				tType: 0xd9,
+				val:   "en",
+			},
+			{
+				tType: 0x00da,
+				val:   uint16(0),
+			},
+		},
+	}
+
+	roomBuf := &bytes.Buffer{}
+	if err := binary.Write(roomBuf, binary.BigEndian, uint16(4)); err != nil {
+		return err
+	}
+	if err := xchange.write(roomBuf); err != nil {
+		return err
+	}
+
 	snacPayloadOut := &TLVPayload{
 		TLVs: []*TLV{
 			{
 				tType: 0x02,
-				val:   uint8(1),
+				val:   uint8(10),
 			},
 			{
 				tType: 0x03,
-				val:   []byte{},
+				val:   roomBuf.Bytes(),
 			},
 		},
 	}
@@ -181,7 +387,7 @@ func SendAndReceiveCreateRoom(flap *flapFrame, snac *snacFrame, r io.Reader, w i
 			},
 			{
 				tType: 0x00d5,
-				val:   uint8(1),
+				val:   uint8(2),
 			},
 		},
 	}
